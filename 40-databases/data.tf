900d8e9d54d35458dfcba5171af7721ac87196b0
siva2j22 <siva2j22@gmail.com>
2025-12-23 19:41:45 +0530

    files

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -27,8 +27,19 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
 
 data "aws_ssm_parameter" "redis_sg_id" {
     name = "/${var.project_name}/${var.environment}/redis_sg_id"
+}
+
+    data "aws_ssm_parameter" "rabbitmq_subnet_id" {
+    name = "/${var.project_name}/${var.environment}/rabbitmq_subnet_id"
 
 }
+
+data "aws_ssm_parameter" "mysql_subnet_id" {
+    name = "/${var.project_name}/${var.environment}/mysql_subnet_id"
+
+}
+
+
 
 data "aws_ssm_parameter" "database_subnet_ids" {
     name = "/${var.project_name}/${var.environment}/database_subnet_ids"
@@ -36,12 +47,3 @@ data "aws_ssm_parameter" "database_subnet_ids" {
 }
 
 
-data "aws_ssm_parameter" "rabbitmq_subnet_ids" {
-    name = "/${var.project_name}/${var.environment}/rabbitmq_subnet_ids"
-
-}
-
-data "aws_ssm_parameter" "mysql_subnet_ids" {
-    name = "/${var.project_name}/${var.environment}/mysql_subnet_ids"
-
-}
