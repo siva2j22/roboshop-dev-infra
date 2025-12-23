21a2d6d838e3eb86cf7de0326c9909f978794232
siva2j22 <siva2j22@gmail.com>
2025-12-23 17:16:49 +0530

    files update

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -2,4 +2,5 @@ locals {
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
   bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
   #public_subnets_ids = data.aws_ssm_parameter.public_subnets_ids.value
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value 
 }
