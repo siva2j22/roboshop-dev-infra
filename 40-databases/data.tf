730773898cd943ab9dfb38dd35ab823022a9ee06
siva2j22 <siva2j22@gmail.com>
2025-12-24 16:47:15 +0530

    files changed

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -31,6 +31,10 @@ data "aws_ssm_parameter" "rabbitmq_sg_id" {
     name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id"
 }
 
+data "aws_ssm_parameter" "mysql_sg_id" {
+    name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+}
+
 
 #  data "aws_ssm_parameter" "rabbitmq_subnet_id" {
 #     name = "/${var.project_name}/${var.environment}/rabbitmq_subnet_id"
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -24,3 +24,6 @@ variable "sg_names" {
 
     
 }
+
+
+  
