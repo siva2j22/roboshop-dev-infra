c59025e2d868921e241a989b32705d7d6c610d53
siva2j22 <siva2j22@gmail.com>
2025-12-24 12:22:24 +0530

    pushed

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mongodb" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.mongodb_sg_id]
-    subnet_id = local.database_subnet_id
+    subnet_id = local.database_subnets_id
     
     tags = merge (
         local.common_tags,
