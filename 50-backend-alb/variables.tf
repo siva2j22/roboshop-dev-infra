0bb6ad189d8155ccbb28425cddedec4bc99b239b
siva2j22 <siva2j22@gmail.com>
2025-12-29 18:56:51 +0530

    route53 records for alb

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0462684FV5U34P7ASOD"
+}
+
+variable "domain_name" {
+    default = "dawsjakkam.fun"
+}
