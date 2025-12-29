f822441ed42922c355655f3b1962de702f162e8a
siva2j22 <siva2j22@gmail.com>
2025-12-29 18:29:30 +0530

    code update

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -4,7 +4,7 @@ locals {
   private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   ami_id = data.aws_ami.joindevops.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-#  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
  # backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   common_tags = {
       Project = var.project_name
