98493b4cbf6d8449e9ec196afc6f7ba153562148
siva2j22 <siva2j22@gmail.com>
2025-12-30 17:05:54 +0530

    update

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -41,7 +41,7 @@ resource "aws_route53_record" "frontend_alb" {
 
   alias {
     # These are ALB details, not our domain details
-    name                   = aws_lb.frontend_alb.domain_name
+   name                   = aws_lb.frontend_alb.dns_name
     zone_id                = aws_lb.frontend_alb.zone_id
     evaluate_target_health = true
   }
