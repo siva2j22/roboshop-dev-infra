05802c12aca3b513ef51923f0eea03c24378d4e0
siva2j22 <siva2j22@gmail.com>
2025-12-30 16:58:54 +0530

    update file

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -16,7 +16,7 @@ resource "aws_lb" "frontend_alb" {
 }
 
 resource "aws_lb_listener" "frontend_alb" {
-  load_balancer_arn = aws_lb.frontend-alb.arn
+  load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
